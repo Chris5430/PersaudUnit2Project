5b8942dcbf054fbb4a5aac4a75e0137b3211bfc9
Christopher Persaud <christopherp543@nycstudents.net>
2022-10-18 12:33:22 -0400

    c

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome!");
+        System.out.println("Enter coordinate 1: ");
+        String coord1 = input.nextLine();
+        System.out.println("Enter coordinate 2: ");
+        String coord2 = input.nextLine();
+        System.out.println();
+        System.out.println("The two points are: " + coord1 + " and " + coord2);
+        int x1 = Integer.parseInt(coord1,1);
+        int y1 = Integer.parseInt(coord1, coord1.indexOf(x1) + (x1 + "").length());
+        int x2 = Integer.parseInt(coord2, 1);
+        int y2 = Integer.parseInt(coord2, coord2.indexOf(x2) + (x2 + "").length());
+        if((x2-x1)==0){
+            System.out.println("The coordinates entered result in a vertical line along x = " + x2 + " for points " + coord1 + " and " + coord2);
+
+        }else {
+            LinearEquation line = new LinearEquation(x1, y1, x2, y2);
+        }
+    }
 }
